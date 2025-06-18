07bbe8aad50b28b31b05a42e21a518d0d1e47dec
Veera Narayana <veeranarayanavra@gmail.com>
2025-06-18 15:50:32 +0530

    Initial commit with frontend and backend

---
+++ index.js
@@ -0,0 +1,38 @@
+
+
+const express = require('express');
+const mongoose = require('mongoose');
+const Routers = require('./Routers/routers');
+const cors = require('cors');
+const errorHandler = require('./middleware/errorHandler');
+const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:5173',       // ✅ allow your frontend
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+    credentials: true                      // ✅ if using cookies or auth headers
+}));
+
+app.use(express.json());
+
+
+app.use("/api", Routers);
+
+
+app.use(errorHandler);
+
+// MongoDB connection
+mongoose.connect("mongodb+srv://veeranarayanavra:veera123@cluster0.cbmad.mongodb.net/FlightBooking?retryWrites=true&w=majority&appName=Cluster0")
+    .then(() => {
+        console.log('MongoDB connected successfully');
+        app.listen(3000, () => {
+            console.log('Server is running on port 3000');
+        });
+    })
+    .catch((error) => {
+        console.log('MongoDB connection failed', error);
+    });
+
+
+
